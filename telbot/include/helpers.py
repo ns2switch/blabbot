f3032b06f30040d55fa79157d2341ae16fd508da
Anibal Cañada <anibal@n2h4.es>
2022-01-01 22:02:30 +0100

    v0.15 - minor sync changes

---
+++ telbot/include/helpers.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+def date_format(date):
+    if type(date) is datetime:
+        return date.strftime("%Y-%m-%d %H:%M:%S")
+
+def channel_to_dynamo(data,channel,user):
+    data_dict = data.to_dict()
+    chan_dict = {}
+    chan_dict['telegram'] = str (data_dict['id']) + "-chan-" + str (channel)
+    chan_dict['date'] = date_format(data_dict['date'])
+    chan_dict['message'] = data_dict['message']
+    chan_dict['user'] = str(user)
+    chan_dict['channel'] = str(channel)
+    chan_dict['mentioned'] = data_dict['mentioned']
+    chan_dict['media'] = data_dict['media']
+    chan_dict['from_id'] = data_dict['from_id']
+    return chan_dict
+
+def user_to_dynamo(data,user):
+    data_dict = data.to_dict()
+    chan_dict = {}
+    chan_dict['telegram'] = str (data_dict['id']) + "-user-" + str (user)
+    chan_dict['date'] = date_format(data_dict['date'])
+    chan_dict['message'] = data_dict['message']
+    chan_dict['user'] = str(user)
+    chan_dict['media'] = data_dict['media']
+    chan_dict['from_id'] = data_dict['from_id']
+    return chan_dict
+
+def log_to_dynamo(data,user):
+    data_dict = data.to_dict()
+    chan_dict = {}
+    chan_dict['telegram'] = str (data_dict['id']) + "-log-" + str (user)
+    chan_dict['date'] = date_format(data_dict['date'])
+    chan_dict['message'] = data_dict['message']
+    chan_dict['user'] = str(user)
+    chan_dict['media'] = data_dict['media']
+    chan_dict['from_id'] = data_dict['from_id']
+    return chan_dict
